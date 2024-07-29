a89806930d8a406b92d82d05b00b5caf6443d95b
KiLeXcs <kirill.a.balan@gmail.com>
2024-07-29 23:21:26 +0300

    bad code of website

--- website.py
+++ website.py
@@ -1,1 +1,21 @@
+from flask import Flask, request
+app = Flask(__name__)
 
+info = []
+
+@app.route('/save', methods = ["POST"])
+def save():
+    data = {"info": request.json["info"]}
+    info.append(data)
+    return "Данные сохранены"
+
+@app.route('/read/<int:number>', methods = ["GET"])
+def read(number):
+    return info[number-1]
+
+@app.route('/readall', methods = ["GET"])
+def readall():
+    return info
+
+if __name__ == '__main__':
+   app.run(debug = True)
