7d36cdcd1c72646584e949f2af24dab1511d727a
KiLeXcs <kirill.a.balan@gmail.com>
2024-07-31 21:06:45 +0300

    Fix bag and do tests

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+print ("Просто обращаемся на главную страницу---" + requests.get("http://127.0.0.1:5000/").text)
+print ("Пытаемся прочитать данные, которые еще не записаны---" + requests.get("http://127.0.0.1:5000/read/1").text)
+print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").text)
+print ("Пытаемся удалить данные, которые еще не записаны---" +requests.get("http://127.0.0.1:5000/delete/1").text)
+print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").text)
+
+for i in range (10):
+    data = {"info": i}
+    print (f"Сохраняем {i}---" + requests.post("http://127.0.0.1:5000/save", json = data).text)
+
+print ("Пытаемся прочитать данные 1 ячейки---" + requests.get("http://127.0.0.1:5000/read/1").text)
+print ("Пытаемся прочитать данные 6 ячейки)---" + requests.get("http://127.0.0.1:5000/read/6").text)
+print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").text)
+print ("Пытаемся удалить данные 1 ячейки---" +requests.get("http://127.0.0.1:5000/delete/1").text)
+print ("Пытаемся прочитать сразу все данные после удаления---" + requests.get("http://127.0.0.1:5000/readall").text)
+print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").text)
+print ("Все удалилось---" + requests.get("http://127.0.0.1:5000/readall").text)
--- website.py
+++ website.py
@@ -3,6 +3,19 @@ app = Flask(__name__)
 
 info = []
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Такой страницы не существует"
+
+def check(number):
+    if number > len(info) or number == 0:
+        return False
+    return True
+
+@app.route('/', methods = ["GET"])
+def main():
+    return "Главная страница"
+
 @app.route('/save', methods = ["POST"])
 def save():
     data = {"info": request.json["info"]}
@@ -11,22 +24,32 @@ def save():
 
 @app.route('/read/<int:number>', methods = ["GET"])
 def read(number):
-    return info[number-1]
+    if check(number):
+        return info[number-1]
+    else:
+        return "Таких данных не существует"
 
 @app.route('/delete/<int:number>', methods = ["GET"])
 def delete(number):
-    info.remove(info[number-1])
-    return f"Данные ячейки {number} удалены"
+    if check(number):
+        info.remove(info[number-1])
+        return f"Данные ячейки {number} удалены"
+    else:
+        return "Таких данных не существует"
 
 @app.route('/readall', methods = ["GET"])
 def readall():
-    return info
+    if len(info) > 0:
+        return info
+    return "Никаких данных не записано"
 
 @app.route('/deleteall', methods = ["GET"])
 def deleteall():
     global info
-    info = []
-    return "Все данные удалены"
+    if len(info) != 0:
+        info = []
+        return "Все данные удалены"
+    return "Никакие данные не были записаны"
 
 if __name__ == '__main__':
    app.run(debug = True)
