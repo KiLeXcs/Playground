c46cc63def0923456f0d36e917895c9a6f2add7e
KiLeXcs <kirill.a.balan@gmail.com>
2024-08-06 22:41:05 +0300

    Fix bugs and do normal save data

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ print ("Пытаемся удалить данные, которые еще не
 print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").text)
 
 for i in range (10):
-    data = {"info": i}
+    data = {"Info": i}
     print (f"Сохраняем {i}---" + requests.post("http://127.0.0.1:5000/save", json = data).text)
 
 print ("Пытаемся прочитать данные 1 ячейки---" + requests.get("http://127.0.0.1:5000/read/1").text)
--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 
@@ -19,16 +19,17 @@ def main():
 
 @app.route('/save', methods = ["POST"])
 def save():
-    data = {"Info": request.json["info"]}
+    data = request.json["Info"]
+    print (data)
     info.append(data)
     return json.loads('{"Info": "Data saved", "Status": "OK"}')
 
 @app.route('/read/<int:number>', methods = ["GET"])
 def read(number):
     if check(number):
-        return info[number-1]
+        return jsonify({"Info": info[number-1], "Status": "OK"})
     else:
-        return json.loads('{"Info": "No data on this key", "Status": "OK"}')
+        return json.loads('{"Info": "No data on this key", "Status": "ERROR"}')
 
 @app.route('/delete/<int:number>', methods = ["GET"])
 def delete(number):
@@ -36,13 +37,13 @@ def delete(number):
         info.remove(info[number-1])
         return json.loads('{"Info": "Cell ' + str(number) +' data removed", "Status": "OK"}')
     else:
-        return json.loads('{"Info": "This data does not exist", "Status": "OK"}')
+        return json.loads('{"Info": "This data does not exist", "Status": "ERROR"}')
 
 @app.route('/readall', methods = ["GET"])
 def readall():
     if len(info) > 0:
-        return info
-    return json.loads('{"Info": "No data exist", "Status": "OK"}')
+        return jsonify({"Info": info, "Status": "OK"})
+    return json.loads('{"Info": "No data exist", "Status": "ERROR"}')
 
 @app.route('/deleteall', methods = ["GET"])
 def deleteall():
@@ -50,7 +51,7 @@ def deleteall():
     if len(info) != 0:
         info = []
         return json.loads('{"Info": "All data deleted", "Status": "OK"}')
-    return json.loads('{"Info": "No data was recorded", "Status": "OK"}')
+    return json.loads('{"Info": "No data was recorded", "Status": "ERROR"}')
 
 if __name__ == '__main__':
    app.run(debug = True)
