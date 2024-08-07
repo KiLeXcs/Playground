df6f93e0346dbf956aea8ee4be71a7f202a1faec
KiLeXcs <kirill.a.balan@gmail.com>
2024-08-07 13:33:02 +0300

    Do semi-auto test

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import requests
 
-print ("Просто обращаемся на главную страницу---" + requests.get("http://127.0.0.1:5000/").text)
-print ("Пытаемся прочитать данные, которые еще не записаны---" + requests.get("http://127.0.0.1:5000/read/1").text)
-print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").text)
-print ("Пытаемся удалить данные, которые еще не записаны---" +requests.get("http://127.0.0.1:5000/delete/1").text)
-print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").text)
+print ("Просто обращаемся на главную страницу---" + requests.get("http://127.0.0.1:5000/").json()["Status"])
+print ("Пытаемся прочитать данные, которые еще не записаны---" + requests.get("http://127.0.0.1:5000/read/1").json()["Status"])
+print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").json()["Status"])
+print ("Пытаемся удалить данные, которые еще не записаны---" + requests.get("http://127.0.0.1:5000/delete/1").json()["Status"])
+print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").json()["Status"])
 
-for i in range (10):
+for i in range (5):
     data = {"Info": i}
-    print (f"Сохраняем {i}---" + requests.post("http://127.0.0.1:5000/save", json = data).text)
+    print (f"Сохраняем {i}---" + requests.post("http://127.0.0.1:5000/save", json = data).json()["Status"])
 
-print ("Пытаемся прочитать данные 1 ячейки---" + requests.get("http://127.0.0.1:5000/read/1").text)
-print ("Пытаемся прочитать данные 6 ячейки)---" + requests.get("http://127.0.0.1:5000/read/6").text)
-print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").text)
-print ("Пытаемся удалить данные 1 ячейки---" +requests.get("http://127.0.0.1:5000/delete/1").text)
-print ("Пытаемся прочитать сразу все данные после удаления---" + requests.get("http://127.0.0.1:5000/readall").text)
-print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").text)
-print ("Все удалилось---" + requests.get("http://127.0.0.1:5000/readall").text)
+print ("Пытаемся прочитать данные 1 ячейки---" + requests.get("http://127.0.0.1:5000/read/1").json()["Status"])
+print ("Пытаемся прочитать данные 6 ячейки---" + requests.get("http://127.0.0.1:5000/read/6").json()["Status"])
+print ("Пытаемся прочитать сразу все данные---" + requests.get("http://127.0.0.1:5000/readall").json()["Status"])
+print ("Пытаемся удалить данные 1 ячейки---" +requests.get("http://127.0.0.1:5000/delete/1").json()["Status"])
+print ("Пытаемся прочитать сразу все данные после удаления---" + requests.get("http://127.0.0.1:5000/readall").json()["Status"])
+print ("Пытаемся удалить сразу все данные---" + requests.get("http://127.0.0.1:5000/deleteall").json()["Status"])
+print ("Пытаемся прочитать сразу все данные---" + (requests.get("http://127.0.0.1:5000/readall").json()["Status"]))
